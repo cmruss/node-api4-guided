afed1268b3e43f9da4bf73c17ef2ea179283274a
cmruss <cmrussell08@gmail.com>
2020-01-16 13:10:49 -0500

    added dynamic port npm start

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const server = require('./api/server.js');
 
-const port = 4001;
+const port = process.env.PORT || 4001;
 server.listen(port, () => {
   console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
 });
